cda68efbc01611174b6a30e50dab68bb327436a0
Johan Bloemberg <github@ijohan.nl>
2017-02-22 23:06:12 +0100

    Add some more basic tests.

---
+++ tests/test_organizations.py
@@ -0,0 +1,11 @@
+from failmap_admin.fail.models import Organization, OrganizationType
+
+
+def test_create_organization(db):
+    """Organization can be created."""
+
+    org = Organization(name="test", type=OrganizationType.objects.get(pk=1))
+
+    assert org
+    assert org.name == 'test'
+    assert org.type.name == 'municipality'
