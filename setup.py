8b8596e5da4d39c60e5456708816b1df50656f58
Johan Bloemberg <github@ijohan.nl>
2017-02-22 22:51:18 +0100

    Fix install.

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name='failmap-admin',
     version='0.1',
-    packages=find_packages('.'),
+    packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
     entry_points={
         'console_scripts': [
             'failmap-admin = failmap_admin.manage:main',
         ],
-    }
+    },
+    include_package_data=True,
 )
