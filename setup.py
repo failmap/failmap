57f022138ba131a97afdae98b5758aec86f3ef79
Johan Bloemberg <github@ijohan.nl>
2017-02-22 22:44:43 +0100

    Packaging.

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='failmap-admin',
     version='0.1',
-    packages=find_packages('admin', 'fail'),
+    packages=find_packages('.'),
+    install_requires=open('requirements.txt').readlines(),
+    entry_points={
+        'console_scripts': [
+            'failmap-admin = failmap_admin.manage:main',
+        ],
+    }
 )
