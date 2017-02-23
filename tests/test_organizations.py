18ae95e1363ba955397c31d794d10e8865d7b61e
Johan Bloemberg <github@ijohan.nl>
2017-02-23 01:08:57 +0100

    Fix tests.

--- tests/test_organizations.py
+++ tests/test_organizations.py
@@ -1,4 +1,4 @@
-from failmap_admin.fail.models import Organization, OrganizationType
+from failmap_admin.organizations.models import Organization, OrganizationType
 
 
 def test_create_organization(db):
