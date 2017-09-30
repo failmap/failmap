50c461ed06fa6acae64c82935c6d16f63017116a
Stitch <elger.jonker@gmail.com>
2017-09-30 14:37:31 +0200

    javascript cleanup begin, caching, separating walls of text, trying failing urls (impossible), small fixes

--- failmap_admin/map/urls.py
+++ failmap_admin/map/urls.py
@@ -2,13 +2,15 @@
 from django.conf.urls import url
 
 from failmap_admin.map.views import (index, map_data, organization_report, stats, topfail,
-                                     wanted_urls, topwin)
+                                     wanted_urls, topwin, terrible_urls)
 
 urlpatterns = [
     url(r'^data/map/(?P<weeks_back>[0-9]{0,2})', map_data, name='map data'),
     url(r'^data/stats/(?P<weeks_back>[0-9]{0,2})', stats, name='stats'),
     url(r'^data/topfail/(?P<weeks_back>[0-9]{0,2})', topfail, name='top fail'),
     url(r'^data/topwin/(?P<weeks_back>[0-9]{0,2})', topwin, name='top win'),
+    # disabled until the url ratings are improved to reflect dead endpoints and such too(!)
+    # url(r'^data/terrible_urls/(?P<weeks_back>[0-9]{0,2})', terrible_urls, name='terrible urls'),
     url(r'^data/wanted/', wanted_urls, name='wanted urls'),
     url(r'^data/report/(?P<organization_id>[0-9]{0,200})/(?P<weeks_back>[0-9]{0,2})$',
         organization_report, name='organization report'),
