e9047e7a2260ac45969c641d3ec944e16823e876
Johan Bloemberg <github@ijohan.nl>
2017-10-06 23:23:33 +0200

    Provide default to not fail split.

--- failmap_admin/settings.py
+++ failmap_admin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'ditisgeengeheimvriendachtjedatditeenw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 INTERNAL_IPS = [
     '127.0.0.1'
