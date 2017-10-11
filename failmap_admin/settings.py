9a12447ddae7885d4480d2f4a34cad16d11c4191
Johan Bloemberg <github@ijohan.nl>
2017-10-11 19:31:06 +0200

    Disable debug by default.

--- failmap_admin/settings.py
+++ failmap_admin/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'ditisgeengeheimvriendachtjedatditeenwachtwoordwas')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,::1').split(',')
 
