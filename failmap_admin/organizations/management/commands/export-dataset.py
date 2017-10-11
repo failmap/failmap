f26227100c2a2bb46394dedfe965b13fb55f4870
Stitch <elger.jonker@gmail.com>
2017-10-11 16:42:11 +0200

    updated readme, faster ratings rebuild

--- failmap_admin/organizations/management/commands/export-dataset.py
+++ failmap_admin/organizations/management/commands/export-dataset.py
@@ -58,32 +58,13 @@ class Command(DumpDataCommand):
 
         # this only works for ssqlite.
         if "sqlite" in settings.DATABASES["default"]["ENGINE"]:
-            # http://www.sqlite.org/pragma.html#pragma_foreign_key_check
-            logger.info(
-                'Checking for foreign key issues, and generating possible SQL to remediate issues.')
-
-            cursor = connection.cursor()
-            cursor.execute('''PRAGMA foreign_key_check;''')
-            rows = cursor.fetchall()
-            if rows:
-                logger.error("Cannot create export. There are incomplete foreign keys. "
-                             "See information above to fix this. "
-                             "Please fix these issues manually and try again.")
-
-            for row in rows:
-                logger.info("%25s %6s %25s %6s" % (row[0], row[1], row[2], row[3]))
-
-            logger.error(
-                "Here are some extremely crude SQL statements that might help fix the problem.")
-            for row in rows:
-                logger.info("DELETE FROM %s WHERE id = \"%s\";" % (row[0], row[1]))
-
-            if rows:
+            if not sqlite_has_correct_referential_integrity():
+                logger.error("Fix above issues. Not proceeding.")
                 return
         else:
-            logger.warning("This export might have incorrect integrity: no foreign key check for "
+            logger.error("This export might have incorrect integrity: no foreign key check for "
                            "this engine was implemented. Loaddata might not accept this import. "
-                           "Perform a key check manually.")
+                           "Perform a key check manually and then alter this code to continue.")
             return
 
         filename = "failmap_dataset_%s.yaml" % datetime.now(pytz.utc).strftime("%Y%m%d_%H%M%S")
@@ -115,3 +96,29 @@ class Command(DumpDataCommand):
         logger.debug(options)
 
         super(Command, self).handle(*app_labels, **options)
+
+
+def sqlite_has_correct_referential_integrity():
+    # http://www.sqlite.org/pragma.html#pragma_foreign_key_check
+    logger.info(
+        'Checking for foreign key issues, and generating possible SQL to remediate issues.')
+
+    cursor = connection.cursor()
+    cursor.execute('''PRAGMA foreign_key_check;''')
+    rows = cursor.fetchall()
+    if rows:
+        logger.error("Cannot create export. There are incomplete foreign keys. "
+                     "See information above to fix this. "
+                     "Please fix these issues manually and try again.")
+
+    for row in rows:
+        logger.info("%25s %6s %25s %6s" % (row[0], row[1], row[2], row[3]))
+
+    logger.error(
+        "Here are some extremely crude SQL statements that might help fix the problem.")
+    for row in rows:
+        logger.info("DELETE FROM %s WHERE id = \"%s\";" % (row[0], row[1]))
+
+    if rows:
+        return False
+    return True
