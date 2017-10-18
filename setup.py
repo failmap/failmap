df1b135373cc4c92b16bb7f84e04865cb9e18fb5
Johan Bloemberg <github@ijohan.nl>
2017-10-18 22:17:32 +0200

    automatically determine version

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
+from subprocess import check_output
 from setuptools import find_packages, setup
+
+
+def get_git_version():
+    """Determine current version number from nearest git tag."""
+    try:
+        version = check_output(["git", "describe", "--tags", "--abbrev=0"]).rstrip().decode()
+        develop = bool(check_output(["git", "status", "--porcelain"]).strip())
+        if develop:
+            return version + '.dev'
+        else:
+            return version
+    except:
+        return '0.0.0'
+
 
 setup(
     name='failmap-admin',
-    version='0.1',
+    version=get_git_version(),
     packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
     entry_points={
