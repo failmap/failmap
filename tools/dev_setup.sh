f3992cbe99a1d6197595090b7b714279e3f8372f
Johan Bloemberg <github@ijohan.nl>
2017-10-30 21:05:55 +0100

    Enforce virtualenv.

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -4,6 +4,11 @@ set -ve
 
 # run all commands needed to setup dev. environment (like mentioned in README.md)
 
+if -z "$VIRTUAL_ENV"; then
+  echo "Refusing to run with a Virtualenv, please see README.md->Direnv/Virtualenv."
+  exit 1
+fi
+
 pip3 install -e .
 failmap-admin migrate
 # load development environment users/settings
