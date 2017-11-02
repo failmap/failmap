6dd073fb651125b1a7e5a8527db93ea92b92937f
Johan Bloemberg <github@ijohan.nl>
2017-11-02 23:45:16 +0100

    Register tasks.

--- failmap/scanners/tasks.py
+++ failmap/scanners/tasks.py
@@ -1,5 +1,5 @@
 """Import modules containing tasks that need to be auto-discovered by Django Celery."""
-from . import scanner_security_headers, scanner_tls_qualys
+from . import scanner_dummy_dumdum, scanner_security_headers, scanner_tls_qualys
 
 # explicitly declare the imported modules as this modules 'content', prevents pyflakes issues
-__all__ = [scanner_tls_qualys, scanner_security_headers]
+__all__ = [scanner_tls_qualys, scanner_security_headers, scanner_dummy_dumdum]
