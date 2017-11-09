fb2ffeb63d7e1f2a22504eea17c320d97566da6a
Stitch <elger.jonker@gmail.com>
2017-11-09 17:13:36 +0100

    icons on buttons, improved listing to determine actions

--- failmap_admin/organizations/admin.py
+++ failmap_admin/organizations/admin.py
@@ -1,23 +1,25 @@
-from datetime import datetime  # admin functions
+from datetime import datetime
 
-import pytz  # admin functions
+import pytz
+import logging
 from django.contrib import admin
 from jet.admin import CompactInline
 
 from ..app.models import Job
-from ..map.determineratings import OrganizationRating, UrlRating, rate_organization, rate_urls
-from ..scanners.models import Endpoint
-from ..scanners.scanner_dns import brute_known_subdomains, certificate_transparency
-from ..scanners.scanner_http import scan_urls_on_standard_ports
-from ..scanners.scanner_plain_http import scan_urls as plain_http_scan_urls
-from ..scanners.scanner_screenshot import screenshot_urls
-from ..scanners.scanner_security_headers import scan_urls as security_headers_scan_urls
-from ..scanners.scanner_tls_qualys import ScannerTlsQualys
+
+from failmap_admin.map.determineratings import (OrganizationRating, UrlRating, rate_organization,
+                                                rate_selected_organizations, rate_urls)
+from failmap_admin.scanners.models import Endpoint
+from failmap_admin.scanners.scanner_dns import brute_known_subdomains, certificate_transparency
+from failmap_admin.scanners.scanner_http import scan_urls_on_standard_ports
+from failmap_admin.scanners.scanner_plain_http import scan_urls as plain_http_scan_urls
+from failmap_admin.scanners.scanner_screenshot import screenshot_urls
+from failmap_admin.scanners.scanner_security_headers import scan_urls as security_headers_scan_urls
+from failmap_admin.scanners.scanner_tls_qualys import ScannerTlsQualys
 from .models import Coordinate, Organization, OrganizationType, Url
 
-# Solved: http://stackoverflow.com/questions/11754877/
-#   troubleshooting-related-field-has-invalid-lookup-icontains
-#   while correct, error should point to ModelAdmin.search fields documentation
+
+logger = logging.getLogger(__name__)
 
 
 class UrlAdminInline(CompactInline):
@@ -97,14 +99,14 @@ class UrlAdmin(admin.ModelAdmin):
     class Media:
         js = ('js/action_buttons.js', )
 
-    list_display = ('url', 'is_dead_reason', 'not_resolvable', 'created_on')
+    list_display = ('url', 'endpoints', 'onboarded', 'uses_dns_wildcard', 'is_dead', 'not_resolvable')
     search_fields = ('url', )
     list_filter = ('url', 'is_dead', 'is_dead_since', 'is_dead_reason',
                    'not_resolvable', 'uses_dns_wildcard', 'organization')
 
     fieldsets = (
         (None, {
-            'fields': ('url', 'organization', 'created_on')
+            'fields': ('url', 'organization', 'created_on', 'onboarded')
         }),
         ('DNS', {
             'fields': ('uses_dns_wildcard', ),
@@ -116,42 +118,56 @@ class UrlAdmin(admin.ModelAdmin):
             'fields': ('is_dead', 'is_dead_since', 'is_dead_reason'),
         }),
     )
-    readonly_fields = ['created_on']
+    readonly_fields = ['created_on', 'onboarded']
 
-    def is_dead(self):
-        if self.something == '1':
-            return True
-        return False
-
-    is_dead.boolean = True
-    is_dead = property(is_dead)
+    def endpoints(self, obj: Url):
+        return obj.endpoint_set.count()
 
     inlines = [EndpointAdminInline, UrlRatingAdminInline]
 
     actions = []
 
+    def onboard(self, request, queryset):
+        # todo, sequentially doesn't matter if you only use tasks :)
+        # currently it might crash given there are no endpoints yet to process...
+        for url in queryset:
+            if url.is_top_level():
+                brute_known_subdomains([url])
+                certificate_transparency([url])
+            scan_urls_on_standard_ports([url])  # discover endpoints
+            plain_http_scan_urls([url])  # see if there is missing https
+            security_headers_scan_urls([url])
+            screenshot_urls([url])
+
+            url.onboarded = True
+            url.onboarded_on = datetime.now(pytz.utc)
+            url.save()
+        self.message_user(request, "Onboard: Done")
+    actions.append('onboard')
+    onboard.short_description = "🔮  Onboard (dns, endpoints, scans, screenshot)"
+
     def dns_certificate_transparency(self, request, queryset):
         certificate_transparency([url for url in queryset])
         self.message_user(request, "URL(s) have been scanned on known subdomains: Done")
     actions.append('dns_certificate_transparency')
-    dns_certificate_transparency.short_description = "Discover subdomains (using certificate transparency)"
+    dns_certificate_transparency.short_description = "🗺  Discover subdomains (using certificate transparency)"
 
     def dns_known_subdomains(self, request, queryset):
         brute_known_subdomains([url for url in queryset])
         self.message_user(request, "Discover subdomains (using known subdomains): Done")
-    dns_known_subdomains.short_description = "Discover subdomains (using known subdomains)"
+    dns_known_subdomains.short_description = "🗺  Discover subdomains (using known subdomains)"
     actions.append('dns_known_subdomains')
 
     def discover_http_endpoints(self, request, queryset):
         scan_urls_on_standard_ports([url for url in queryset])
         self.message_user(request, "Discover http(s) endpoints: Done")
-    discover_http_endpoints.short_description = "Discover http(s) endpoints"
+    discover_http_endpoints.short_description = "🗺  Discover http(s) endpoints"
     actions.append('discover_http_endpoints')
 
     def scan_tls_qualys(self, request, queryset):
         ScannerTlsQualys().scan([url.url for url in queryset])
         self.message_user(request, "Scan TLS (qualys, slow): Scheduled with Priority")
-    scan_tls_qualys.short_description = "Scan TLS (qualys, slow)"
+    scan_tls_qualys.short_description = "🔬  Scan TLS (qualys, slow)"
     actions.append('scan_tls_qualys')
 
     def security_headers(self, request, queryset):
@@ -161,31 +177,32 @@ class UrlAdmin(admin.ModelAdmin):
         name = "Scan Security Headers (%s) " % str(urls)
         job = Job.create(task, name, request)
         self.message_user(request, "%s: job created, id:%s" % (name, str(job)))
-    security_headers.short_description = "Scan Security Headers"
+    security_headers.short_description = "🔬  Scan Security Headers"
     actions.append('security_headers')
 
     def plain_http_scan(self, request, queryset):
         plain_http_scan_urls([url for url in queryset])
         self.message_user(request, "Scan Plain Http: done")
-    plain_http_scan.short_description = "Scan Plain Http"
+    plain_http_scan.short_description = "🔬  Scan Plain Http"
     actions.append('plain_http_scan')
 
     def screenshots(self, request, queryset):
         screenshot_urls([url for url in queryset])
         self.message_user(request, "Create screenshot: Done")
-    screenshots.short_description = "Create screenshot"
+    screenshots.short_description = "📷  Create screenshot"
     actions.append('screenshots')
 
     def rate_url(self, request, queryset):
         rate_urls([url for url in queryset])
         self.message_user(request, "Rate Url: done")
-    rate_url.short_description = "Rate Url"
+    rate_url.short_description = "✅  Rate Url"
     actions.append('rate_url')
 
     def rate_organization_(self, request, queryset):
-        rate_organization([url.organization for url in queryset])
+        print(list(url.organization.all()) for url in queryset)
+        rate_selected_organizations(list(url.organization.all()) for url in queryset)
         self.message_user(request, "Rate Organization: done")
-    rate_organization_.short_description = "Rate Organization"
+    rate_organization_.short_description = "✅  Rate Organization"
     actions.append('rate_organization_')
 
     def declare_dead(self, request, queryset):
@@ -195,7 +212,7 @@ class UrlAdmin(admin.ModelAdmin):
             url.is_dead_since = datetime.now(pytz.utc)
             url.save()
         self.message_user(request, "Declare dead: Done")
-    declare_dead.short_description = "Declare dead"
+    declare_dead.short_description = "🔪  Declare dead"
     actions.append('declare_dead')
 
 
