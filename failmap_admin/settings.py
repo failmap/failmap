d5bb536e95989c385c2270f102a6e5890b77a6ed
Johan Bloemberg <github@ijohan.nl>
2017-11-12 10:55:21 +0100

    Don't include credentials in default config :).

--- failmap_admin/settings.py
+++ failmap_admin/settings.py
@@ -491,7 +491,7 @@ if SENTRY_DSN:
     MIDDLEWARE_CLASSES.insert(0, 'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware')
 
 # set javascript sentry token if provided
-SENTRY_TOKEN = os.environ.get('SENTRY_TOKEN', 'https://a4f72b82fc0742bc82b82560b340006b@sentry.io/242170')
+SENTRY_TOKEN = os.environ.get('SENTRY_TOKEN', '')
 
 SENTRY_ORGANIZATION = 'internet-cleanup-foundation'
 SENTRY_PROJECT = 'faalkaart'
