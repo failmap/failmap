c8c1cbbd26cecb9dc5161a90b1ba51d9563f703f
Johan Bloemberg <github@ijohan.nl>
2017-11-14 21:25:02 +0100

    Add organization ratings to test.

--- tools/show_ratings.sh
+++ tools/show_ratings.sh
@@ -2,11 +2,18 @@
 
 set -x
 
-# output a sorted list of all urls id's and their respective rating
+# output a sorted list of all urls/organizations and their respective rating
 
 failmap-admin shell <<EOF | sort
-from failmap_admin.map.models import UrlRating
-print('\n'.join('{url__url}\t{rating}'.format(**x) for x in UrlRating.objects.all().values('url__url', 'rating')))
+from failmap_admin.map.models import UrlRating, OrganizationRating
+
+# output url and ratings for urls
+for urlrating in UrlRating.objects.all().values('url__url', 'rating'):
+  print('{url__url}\t{rating}'.format(**urlrating))
+
+# output name and rating for organizations
+for organizationrating in OrganizationRating.objects.all().values('organization__name', 'rating'):
+  print('{organization__name}\t{rating}'.format(**organizationrating))
 EOF
 
 
