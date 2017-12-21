36cc46bdf9ef0531732ca9dcf5f9d21a72a60967
Johan Bloemberg <github@ijohan.nl>
2017-12-21 12:12:46 +0100

    Integrationtest basics.

---
+++ tests/integration/README.md
@@ -0,0 +1,1 @@
+Test that require a more complete environment than mere unit tests would. Skipped during normal test.
---
+++ tests/integration/test_celery.py
@@ -0,0 +1,6 @@
+"""Test/prove some assumtions about celery behaviour (eg: priorities)."""
+
+
+def test_priorities():
+    """High prio tasks should be executed first."""
+    assert 0
