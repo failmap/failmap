d5142d1f35e12f54525a69fcc9ac59fbc5eb63bf
Johan Bloemberg <github@ijohan.nl>
2018-01-10 18:00:43 +0100

    Remove hack for rebuild ratings dashboard module.

---
+++ failmap/app/dashboard_module_views.py
@@ -0,0 +1,34 @@
+from django.conf.urls import url
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
+from jet.dashboard import dashboard
+
+from failmap.map.rating import rebuild_ratings as rebuild_ratings_task
+
+from ..celery import PRIO_HIGH
+from .models import Job
+
+
+def rebuild_ratings(request):
+    """Create rebuild ratings task and dispatch using a Job to allow the user to track progress."""
+    name = 'Rebuild ratings'
+    # create a Task signature for rebuilding ratings, wrap this inside a Job
+    # to have it trackable by the user in the admin interface
+    task = rebuild_ratings_task.s()
+    job = Job.create(task, name, request, priority=PRIO_HIGH)
+
+    # tell the user where to find the Job that was just created
+    link = reverse('admin:app_job_change', args=(job.id,))
+    messages.success(request, '%s: job created, id: <a href="%s">%s</a>' % (name, link, str(job)))
+
+    return redirect(reverse('admin:index'))
+
+
+dashboard.urls.register_urls([
+    url(
+        r'^rebuild_ratings/',
+        rebuild_ratings,
+        name='rebuild-ratings'
+    ),
+])
--- failmap/app/dashboard_modules.py
+++ failmap/app/dashboard_modules.py
@@ -5,36 +5,13 @@
 # https://docs.djangoproject.com/en/1.10/intro/overview/#enjoy-the-free-api
 # https://docs.djangoproject.com/en/1.10/topics/db/queries/
 
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
-from django.urls import reverse
 from jet.dashboard.modules import DashboardModule
-
-from failmap.map.rating import rebuild_ratings as rebuild_ratings_task
-
-from ..celery import PRIO_HIGH
-from .models import Job
 
 
 class RebuildRatings(DashboardModule):
     title = 'Rebuild Ratings'
     title_url = 'Rebuild Ratings'
 
-    template = 'app/templates/admin/RebuildRatings.html'
+    template = 'app/templates/admin/rebuild_ratings.html'
 
 
-@login_required
-def rebuild_ratings(request):
-    """Create rebuild ratings task and dispatch using a Job to allow the user to track progress."""
-    name = 'Rebuild ratings'
-    # create a Task signature for rebuilding ratings, wrap this inside a Job
-    # to have it trackable by the user in the admin interface
-    task = rebuild_ratings_task.s()
-    job = Job.create(task, name, request, priority=PRIO_HIGH)
-
-    # tell the user where to find the Job that was just created
-    link = reverse('admin:app_job_change', args=(job.id,))
-    messages.success(request, '%s: job created, id: <a href="%s">%s</a>' % (name, link, str(job)))
-
-    return redirect(reverse('admin:index'))
