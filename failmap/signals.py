b6e456953cd64a188f1215f35781ce74414adfe6
Johan Bloemberg <github@ijohan.nl>
2018-02-12 17:24:56 +0100

    Make multiple workers more distinguisable.

--- failmap/signals.py
+++ failmap/signals.py
@@ -1,6 +1,7 @@
 """This module is imported by failmap.__init__ to register Signal hooks."""
-
 import logging
+import os
+import platform
 import shutil
 import sys
 import tempfile
@@ -14,8 +15,17 @@ log = logging.getLogger(__name__)
 
 
 @celeryd_init.connect
-def configure_workers(sender=None, conf=None, **kwargs):
+def configure_workers(sender=None, conf=None, instance=None, **kwargs):
     """Configure workers when Celery is initialized."""
+
+    # set hostname based on context
+    container_host_name = os.environ.get('HOST_HOSTNAME', None)
+    if container_host_name:
+        hostname = '.'.join([platform.node(), container_host_name])
+    else:
+        hostname = platform.node()
+    role = os.environ.get('WORKER_ROLE', 'default')
+    instance.hostname = "%s@%s" % (role, hostname)
 
     try:
         # create a universal temporary directory to be removed when the application quits
