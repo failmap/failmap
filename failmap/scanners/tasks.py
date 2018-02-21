35ca7db6af9bf1923362a0fac920e18f6b0a82b5
Stitch <elger.jonker@gmail.com>
2018-02-21 15:13:35 +0100

    should be placed here for consistency

--- failmap/scanners/tasks.py
+++ failmap/scanners/tasks.py
@@ -1,5 +1,5 @@
 """Import modules containing tasks that need to be auto-discovered by Django Celery."""
-from . import scanner_dummy, scanner_security_headers, scanner_tls_qualys
+from . import scanner_dummy, scanner_security_headers, scanner_tls_qualys, scanner_http
 
 # explicitly declare the imported modules as this modules 'content', prevents pyflakes issues
-__all__ = [scanner_tls_qualys, scanner_security_headers, scanner_dummy]
+__all__ = [scanner_tls_qualys, scanner_security_headers, scanner_dummy, scanner_http]
