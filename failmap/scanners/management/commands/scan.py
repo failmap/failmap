f176fab175be1560fe1012c8367b004119e33cef
Stitch <elger.jonker@gmail.com>
2018-03-05 10:54:48 +0100

    added generic scan feature: failmap scan [scanner_name].

---
+++ failmap/scanners/management/commands/scan.py
@@ -0,0 +1,35 @@
+import logging
+
+from failmap.app.management.commands._private import ScannerTaskCommand
+from failmap.scanners import (scanner_dns, scanner_dnssec, scanner_http, scanner_plain_http,
+                              scanner_security_headers, scanner_tls_qualys)
+
+log = logging.getLogger(__name__)
+
+
+class Command(ScannerTaskCommand):
+    """Can perform a host of scans. Run like: failmap scan [scanner_name] and then options."""
+
+    help = __doc__
+
+    def add_arguments(self, parser):
+        parser.add_argument('scanner', nargs=1, help='The scanner you want to use.')
+        super().add_arguments(parser)
+
+    def handle(self, *args, **options):
+
+        scanners = {
+            'dnssec': scanner_dnssec,
+            'headers': scanner_security_headers,
+            'plain': scanner_plain_http,
+            'endpoints': scanner_http,
+            'tls': scanner_tls_qualys,
+            'dns': scanner_dns
+        }
+
+        if options['scanner'][0] not in scanners:
+            print("Scanner does not exist. Please specify a scanner: %s " % scanners.keys())
+            return
+
+        self.scanner_module = scanners[options['scanner'][0]]
+        super().handle(self, *args, **options)
