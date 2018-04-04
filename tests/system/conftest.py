29d2dc6b0ffb3a650cca36e65247cbc6f60c04fa
Johan Bloemberg <github@ijohan.nl>
2018-04-04 12:23:48 +0200

    Fix passing longer timeout for CI to system tests, capture more logging from containers (eg: unflushed, shutdown logs)

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -4,12 +4,14 @@ import re
 import subprocess
 import time
 import urllib.request
+from datetime import timedelta
 
 import pytest
 
 log = logging.getLogger(__name__)
 
-TIMEOUT = os.environ.get('TIMEOUT', 30)
+TIMEOUT = timedelta(seconds=int(os.environ.get('SYSTEM_TEST_TIMEOUT', 30)))
+INTERVAL = timedelta(seconds=5)
 
 
 @pytest.fixture(scope='session')
@@ -61,32 +63,36 @@ def docker_services(pytestconfig, docker_ip):
     )
     docker_compose_project_name = "pytest{}".format(os.getpid())
 
-    def docker_compose(args):
+    def docker_compose(args, background=False):
         command = 'docker-compose --no-ansi -f "{}" -p "{}" {}'.format(
             docker_compose_file, docker_compose_project_name, args
         )
         log.info('Running command: %s', command)
         env = dict(os.environ, ALLOWED_HOSTS=docker_ip)
-        return subprocess.check_output(command, env=env, shell=True, universal_newlines=True)
+        if background:
+            return subprocess.Popen(command, env=env, shell=True)
+        else:
+            return subprocess.check_output(command, env=env, shell=True, universal_newlines=True)
 
     docker_compose('up -d')
 
+    # docker-compose binds port 8000 to an unique port number, extract that number here to form url
     url = 'http://%s:%d' % (docker_ip, int(docker_compose('port admin 8000').split(':')[-1]))
 
-    log.info('Waiting for url %s to be responsive.', url)
-    for _ in range(TIMEOUT):
+    print('waiting for url %s to be responsive.' % url)
+    for _ in range(int(TIMEOUT.seconds / INTERVAL.seconds)):
         try:
             # verify admin is http responsive and stop waiting
-            urllib.request.urlopen(url)
+            urllib.request.urlopen('url')
+            print("admin instance is ready for testing")
             break
         except BaseException as e:
-            print("admin instance not accessible, exception: %r" % e)
-            time.sleep(1)
+            print("admin instance not accessible, exception: %r, retrying in %i" % (e, INTERVAL.seconds))
+            time.sleep(INTERVAL.seconds)
     else:
         # provide context as to why the containered service failed to be responsive
         for service in docker_compose('config --services').splitlines():
-            for line in docker_compose('logs %s' % service).splitlines():
-                print(line)
+            docker_compose('logs -f %s' % service, background=True)
         docker_compose('down -v')
         pytest.fail(
             "admin instance at %s not accessible after %s seconds" %
@@ -94,7 +100,7 @@ def docker_services(pytestconfig, docker_ip):
 
     yield docker_compose
 
+    # output logs in case the test failed, and cleanup the containers
     for service in docker_compose('config --services').splitlines():
-        for line in docker_compose('logs %s' % service).splitlines():
-            print(line)
+        docker_compose('logs -f %s' % service, background=True)
     docker_compose('down -v')
