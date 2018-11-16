85278561829bba6d49104a5f24568eb4197317f1
stitch1 <elger.jonker@gmail.com>
2018-11-16 11:41:35 +0100

    Change title of admin interface
    Former-commit-id: a9306606c0e5c6535413d22c10ea577e416dc3c6

--- failmap/urls.py
+++ failmap/urls.py
@@ -28,6 +28,9 @@ import failmap.app.dashboard_module_views  # noqa
 # Django 1.10 http://stackoverflow.com/questions/38744285/
 # django-urls-error-view-must-be-a-callable-or-a-list-tuple-in-the-case-of-includ#38744286
 
+admin.site.site_header = 'Failmap Admin'
+admin.site.site_title = 'Failmap Admin'
+
 admin_urls = [
     url(r'^admin/', admin.site.urls),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
