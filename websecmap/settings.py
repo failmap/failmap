c208dd80b4625a2e5feee8f98840d38a3654b5ff
stitch1 <elger.jonker@gmail.com>
2019-03-26 12:06:03 +0100

    Move constance scanning variables to separate function for easier importing

--- websecmap/settings.py
+++ websecmap/settings.py
@@ -21,7 +21,7 @@ import raven.contrib.celery
 from django.utils.translation import gettext_lazy as _
 from pkg_resources import get_distribution
 
-from websecmap.scanners import SCANNERS
+from websecmap.scanners.constance import add_scanner_fields, add_scanner_fieldsets
 
 __version__ = get_distribution(__name__.split('.', 1)[0]).version
 
@@ -878,47 +878,7 @@ CONSTANCE_CONFIG = {
     'PRO_EMAIL_SSL_CERTFILE': ('', 'See django mail settings.', str),
 }
 
-# Generate Scanner Settings:
-for scanner in SCANNERS:
-
-    scan_types = scanner['creates endpoint scan types'] + scanner['creates url scan types']
-
-    if scan_types:
-        # No scanner options when there are no scan types, those are usually discovery scanners.
-        scanner_config = {
-            'USE_SCANNER_%s' % scanner['name'].upper():
-                (True, 'Do you want to use the %s scanner?' % scanner['name'], bool),
-        }
-        CONSTANCE_CONFIG = {**CONSTANCE_CONFIG, **scanner_config}
-
-    if scanner['can discover endpoints']:
-        scanner_config = {
-            'DISCOVER_ENDPOINTS_USING_%s' % scanner['name'].upper(): (
-                True, 'Do you want to discover endpoints using the %s scanner?' % scanner['name'], bool),
-        }
-        CONSTANCE_CONFIG = {**CONSTANCE_CONFIG, **scanner_config}
-
-    if scanner['can discover urls']:
-        scanner_config = {
-            'DISCOVER_URLS_USING_%s' % scanner['name'].upper(): (
-                True, 'Do you want to discover urls using the %s scanner?' % scanner['name'], bool),
-        }
-        CONSTANCE_CONFIG = {**CONSTANCE_CONFIG, **scanner_config}
-
-    for scan_type in scanner['creates endpoint scan types'] + scanner['creates url scan types']:
-        scanner_config = {
-            'REPORT_INCLUDE_%s' % scan_type.upper():
-                (True, 'Do you want to add %s issues to the report?' % scan_type, bool),
-        }
-        CONSTANCE_CONFIG = {**CONSTANCE_CONFIG, **scanner_config}
-
-    for scan_type in scanner['creates endpoint scan types'] + scanner['creates url scan types']:
-        scanner_config = {
-            'SHOW_%s' % scan_type.upper():
-                (True, 'Do you want to show %s issues on the website?' % scan_type, bool),
-        }
-        CONSTANCE_CONFIG = {**CONSTANCE_CONFIG, **scanner_config}
-
+CONSTANCE_CONFIG = add_scanner_fields(CONSTANCE_CONFIG)
 
 CONSTANCE_CONFIG_FIELDSETS = OrderedDict([
     ('General',
@@ -951,39 +911,8 @@ CONSTANCE_CONFIG_FIELDSETS = OrderedDict([
       )),
 ])
 
-# now create the menus for these scanners.
-discover_set = ('SCAN_AT_ALL', )
-for scanner in SCANNERS:
-    if scanner['can discover urls']:
-        discover_set += ('DISCOVER_URLS_USING_%s' % scanner['name'].upper(), )
 
-for scanner in SCANNERS:
-    if scanner['can discover endpoints']:
-        discover_set += ('DISCOVER_ENDPOINTS_USING_%s' % scanner['name'].upper(), )
-
-scanner_config_set = []
-for scanner in SCANNERS:
-
-    scan_types = scanner['creates endpoint scan types'] + scanner['creates url scan types']
-    if not scan_types:
-        continue
-
-    options = ('USE_SCANNER_%s' % scanner['name'].upper(), )
-
-    for scan_type in scanner['creates endpoint scan types'] + scanner['creates url scan types']:
-        options += ('REPORT_INCLUDE_%s' % scan_type.upper(), )
-
-    for scan_type in scanner['creates endpoint scan types'] + scanner['creates url scan types']:
-        options += ('SHOW_%s' % scan_type.upper(), )
-
-    scanner_config_per_scanner = (scanner['verbose name'], options)
-    scanner_config_set += (scanner_config_per_scanner, )
-
-CONSTANCE_CONFIG_FIELDSETS.update([
-    ('Discovery of new urls, endpoints and scanning', discover_set),
-])
-
-CONSTANCE_CONFIG_FIELDSETS.update(scanner_config_set)
+CONSTANCE_CONFIG_FIELDSETS = add_scanner_fieldsets(CONSTANCE_CONFIG_FIELDSETS)
 
 CONSTANCE_CONFIG_FIELDSETS.update([
     ('Fair / Stand options',
