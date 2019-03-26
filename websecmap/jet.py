6430e1cd870c88c5ef4fade9402bf77f07c52639
stitch1 <elger.jonker@gmail.com>
2019-03-26 13:21:32 +0100

    Move jet menu to function, so it's easily embeddable in other projects

---
+++ websecmap/jet.py
@@ -0,0 +1,83 @@
+from django.utils.translation import gettext_lazy as _
+
+
+def websecmap_menu_items():
+    return [
+
+        {'label': _('🗺️ Map'), 'items': [
+            {'name': 'map.configuration', 'label': _('Map Configuration')},
+            {'name': 'map.administrativeregion', 'label': _('Region Importer')},
+            {'name': 'map.mapdatacache', 'label': 'Map Data Cache (generated)'},
+            {'name': 'map.vulnerabilitystatistic', 'label': 'Statistics (generated)'},
+            {'name': 'map.highlevelstatistic', 'label': 'Organization statistics (generated)'}
+        ], 'permissions': ['admin']},
+
+        {'app_label': 'organizations', 'label': _('🏢 Organizations'), 'items': [
+            {'name': 'organization'},
+            {'name': 'url'},
+            # Promises have been replaced with comply or explain. On feature request they might return in the future.
+            # The code is still in the system for this.
+            # {'name': 'promise'},
+            {'name': 'organizationtype'},
+            {'name': 'dataset', 'label': 'Data Set Import'},
+            {'name': 'coordinate'},
+        ], 'permissions': ['admin']},
+
+        {'label': _('🕒 Periodic Tasks'), 'items': [
+            {'name': 'app.job'},
+            {'name': 'django_celery_beat.periodictask'},
+            {'name': 'django_celery_beat.crontabschedule'},
+            # We support only crontabs as they deliver clear and concise information when the next scan will happen
+            # This is not true for interval and solar, while they are easier to understand and read. It's unfortunate...
+            # {'name': 'django_celery_beat.intervalschedule'},
+            # {'name': 'django_celery_beat.solarschedule'},
+        ], 'permissions': ['admin']},
+
+        {'app_label': 'scanners', 'label': _('🔬 Scanning (generated)'), 'items': [
+            {'name': 'endpoint', 'permissions': ['admin'], 'label': 'Endpoints'},
+            {'name': 'endpointgenericscan', 'permissions': ['scanners.change_endpointgenericscan'],
+             'label': 'Endpoint Scans'},
+            {'name': 'urlgenericscan', 'permissions': ['scanners.change_urlgenericscan'], 'label': 'URL Scans'},
+            {'name': 'internetnlscan', 'permissions': ['scanners.change_internetnlscan'],
+             'label': 'Internet.nl Scans Tasks'},
+            # tlsqualysscans have been merged with endpointgenericscans
+            # {'name': 'tlsqualysscan', 'permissions': ['scanners.change_tlsqualysscan']},
+            {'name': 'scanproxy', 'permissions': ['scanners.scanproxy'], 'label': 'Scan Proxies'},
+            # screenshots are not working yet
+            # {'name': 'screenshot', 'permissions': ['admin']},
+            # UrlIP's are not used currently, they are stored but have no value.
+            # {'name': 'urlip', 'permissions': ['admin']},
+            {'name': 'tlsqualysscratchpad', 'permissions': ['admin'], 'label': 'Qualys Scans Debug'},
+            {'name': 'endpointgenericscanscratchpad', 'permissions': ['admin'],
+             'label': 'Endpoint Scans Debug'},
+        ]},
+
+        {'label': _('📄 Reporting (generated)'), 'items': [
+            {'name': 'reporting.organizationreport'},
+            {'name': 'reporting.urlreport'},
+        ], 'permissions': ['admin']},
+
+        {'app_label': 'pro', 'label': _('⭐ Pro (beta)'), 'items': [
+            {'name': 'account'},
+            {'name': 'creditmutation'},
+            {'name': 'urllist'},
+            {'name': 'urllistreport'},
+            {'name': 'rescanrequest'},
+            {'name': 'failmaporganizationdatafeed'},
+            {'name': 'urldatafeed'},
+        ], 'permissions': ['admin']},
+
+        {'app_label': 'game', 'label': _('👾️ The Game (beta)'), 'items': [
+            {'name': 'contest'},
+            {'name': 'team'},
+            {'name': 'organizationsubmission'},
+            {'label': _('New organizations'),
+             'url': '/admin/game/organizationsubmission/?has_been_accepted__exact=0&has_been_rejected__exact=0&o=-5',
+             'url_blank': False},
+            {'name': 'urlsubmission'},
+            {'label': _('New urls'),
+             'url': '/admin/game/urlsubmission/?has_been_accepted__exact=0&has_been_rejected__exact=0&o=-6.2.3',
+             'url_blank': False},
+        ], 'permissions': ['admin']},
+
+    ]
--- websecmap/settings.py
+++ websecmap/settings.py
@@ -21,6 +21,7 @@ import raven.contrib.celery
 from django.utils.translation import gettext_lazy as _
 from pkg_resources import get_distribution
 
+from websecmap.jet import websecmap_menu_items
 from websecmap.scanners.constance import add_scanner_fields, add_scanner_fieldsets
 
 __version__ = get_distribution(__name__.split('.', 1)[0]).version
@@ -1001,6 +1002,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 # admin (a nonsense permission) has been added everywhere to avoid "empty arrows" when signing in with a role with
 # limited permissions.
 # For the default labels, see: https://docs.djangoproject.com/en/2.1/topics/auth/default/#topic-authorization
+
 JET_SIDE_MENU_ITEMS = [  # A list of application or custom item dicts
 
     {'label': _('🔧 Configuration'), 'items': [
@@ -1008,83 +1010,10 @@ JET_SIDE_MENU_ITEMS = [  # A list of application or custom item dicts
         {'name': 'auth.group'},
         {'name': 'constance.config', 'label': _('Configuration')},
     ], 'permissions': ['admin']},
-
-    {'label': _('🗺️ Map'), 'items': [
-        {'name': 'map.configuration', 'label': _('Map Configuration')},
-        {'name': 'map.administrativeregion', 'label': _('Region Importer')},
-        {'name': 'map.mapdatacache', 'label': 'Map Data Cache (generated)'},
-        {'name': 'map.vulnerabilitystatistic', 'label': 'Statistics (generated)'},
-        {'name': 'map.highlevelstatistic', 'label': 'Organization statistics (generated)'}
-    ], 'permissions': ['admin']},
-
-    {'app_label': 'organizations', 'label': _('🏢 Organizations'), 'items': [
-        {'name': 'organization'},
-        {'name': 'url'},
-        # Promises have been replaced with comply or explain. On feature request they might return in the future.
-        # The code is still in the system for this.
-        # {'name': 'promise'},
-        {'name': 'organizationtype'},
-        {'name': 'dataset', 'label': 'Data Set Import'},
-        {'name': 'coordinate'},
-    ], 'permissions': ['admin']},
-
-    {'label': _('🕒 Periodic Tasks'), 'items': [
-        {'name': 'app.job'},
-        {'name': 'django_celery_beat.periodictask'},
-        {'name': 'django_celery_beat.crontabschedule'},
-        # We support only crontabs as they deliver clear and concise information when the next scan will happen
-        # This is not true for interval and solar, while they are easier to understand and read. It's unfortunate...
-        # {'name': 'django_celery_beat.intervalschedule'},
-        # {'name': 'django_celery_beat.solarschedule'},
-    ], 'permissions': ['admin']},
-
-    {'app_label': 'scanners', 'label': _('🔬 Scanning (generated)'), 'items': [
-        {'name': 'endpoint', 'permissions': ['admin'], 'label': 'Endpoints'},
-        {'name': 'endpointgenericscan', 'permissions': ['scanners.change_endpointgenericscan'],
-         'label': 'Endpoint Scans'},
-        {'name': 'urlgenericscan', 'permissions': ['scanners.change_urlgenericscan'], 'label': 'URL Scans'},
-        {'name': 'internetnlscan', 'permissions': ['scanners.change_internetnlscan'],
-         'label': 'Internet.nl Scans Tasks'},
-        # tlsqualysscans have been merged with endpointgenericscans
-        # {'name': 'tlsqualysscan', 'permissions': ['scanners.change_tlsqualysscan']},
-        {'name': 'scanproxy', 'permissions': ['scanners.scanproxy'], 'label': 'Scan Proxies'},
-        # screenshots are not working yet
-        # {'name': 'screenshot', 'permissions': ['admin']},
-        # UrlIP's are not used currently, they are stored but have no value.
-        # {'name': 'urlip', 'permissions': ['admin']},
-        {'name': 'tlsqualysscratchpad', 'permissions': ['admin'], 'label': 'Qualys Scans Debug'},
-        {'name': 'endpointgenericscanscratchpad', 'permissions': ['admin'],
-         'label': 'Endpoint Scans Debug'},
-    ]},
-
-    {'label': _('📄 Reporting (generated)'), 'items': [
-        {'name': 'reporting.organizationreport'},
-        {'name': 'reporting.urlreport'},
-    ], 'permissions': ['admin']},
-
-    {'app_label': 'pro', 'label': _('⭐ Pro (beta)'), 'items': [
-        {'name': 'account'},
-        {'name': 'creditmutation'},
-        {'name': 'urllist'},
-        {'name': 'urllistreport'},
-        {'name': 'rescanrequest'},
-        {'name': 'failmaporganizationdatafeed'},
-        {'name': 'urldatafeed'},
-    ], 'permissions': ['admin']},
-
-    {'app_label': 'game', 'label': _('👾️ The Game (beta)'), 'items': [
-        {'name': 'contest'},
-        {'name': 'team'},
-        {'name': 'organizationsubmission'},
-        {'label': _('New organizations'),
-         'url': '/admin/game/organizationsubmission/?has_been_accepted__exact=0&has_been_rejected__exact=0&o=-5',
-         'url_blank': False},
-        {'name': 'urlsubmission'},
-        {'label': _('New urls'),
-         'url': '/admin/game/urlsubmission/?has_been_accepted__exact=0&has_been_rejected__exact=0&o=-6.2.3',
-         'url_blank': False},
-    ], 'permissions': ['admin']},
 ]
+
+JET_SIDE_MENU_ITEMS += websecmap_menu_items()
+
 # end django jet menu configuration
 ########
 
