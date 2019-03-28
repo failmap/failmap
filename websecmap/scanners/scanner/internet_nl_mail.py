c1d9cf501e48506a652dfbbf232d3c58b8dfa6e3
stitch1 <elger.jonker@gmail.com>
2019-03-28 14:10:18 +0100

    Better friendly message, return scan instead of ID

--- websecmap/scanners/scanner/internet_nl_mail.py
+++ websecmap/scanners/scanner/internet_nl_mail.py
@@ -233,8 +233,9 @@ def register_scan(urls: List[Url], username, password, internet_nl_scan_type: st
     scan.started_on = datetime.now(pytz.utc)
     scan.started = True
     scan.status_url = status_url
-    scan.message = answer
-    scan.friendly_message = answer.get('message', 'No message received.')
+    scan.message = answer.get('message', 'No message received.')
+    scan.friendly_message = "Batch request has been registered" \
+        if answer.get('OK', False) else "Error when registering batch request"
     scan.type = internet_nl_scan_type
 
     scan.save()
