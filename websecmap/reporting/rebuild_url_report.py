5c2d69faec495928ceb3190cdfc40dd86c67c1c6
stitch1 <elger.jonker@gmail.com>
2019-04-01 18:36:48 +0200

    Add reporting for urls only, ignoring organizations

---
+++ websecmap/reporting/rebuild_url_report.py
@@ -0,0 +1,36 @@
+import logging
+
+from celery import group
+
+from websecmap.celery import Task
+from websecmap.organizations.models import Organization, Url
+from websecmap.reporting.report import recreate_url_reports
+from websecmap.map.report import recreate_organization_reports
+from websecmap.scanners.scanner.__init__ import q_configurations_to_report
+from websecmap.scanners.scanner import add_model_filter
+
+log = logging.getLogger(__package__)
+
+
+def compose_task(
+    **kwargs
+) -> Task:
+
+    urls = Url.objects.filter(q_configurations_to_report())
+    urls = add_model_filter(urls, **kwargs)
+    if not urls:
+        log.error("No urls found.")
+        return group()
+
+    tasks = [recreate_url_reports.si(urls)]
+
+    if not tasks:
+        log.error("Could not rebuild reports, filters resulted in no tasks created.")
+        log.debug("Url filter: %s" % kwargs)
+        return group()
+
+    log.debug("Number of tasks: %s" % len(tasks))
+
+    task = group(tasks)
+
+    return task
