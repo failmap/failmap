8253ddb0d22883de4eae6ed16e4ae120a0418f65
stitch1 <elger.jonker@gmail.com>
2019-04-11 14:13:45 +0200

    Attempt to use less resources in OrganizationReportAdmin

--- websecmap/map/admin.py
+++ websecmap/map/admin.py
@@ -8,6 +8,7 @@ from celery import group
 from django.contrib import admin
 from django.db import transaction
 from django.urls import reverse
+from django.utils.html import format_html
 from import_export.admin import ImportExportModelAdmin
 
 from websecmap.app.models import Job
@@ -394,3 +395,66 @@ class HighLevelStatisticAdmin(ImportExportModelAdmin, admin.ModelAdmin):
 
     instant_cache_clear.short_description = 'Clear all caches (select 1)'
     actions.append(instant_cache_clear)
+
+
+@admin.register(models.OrganizationReport)
+class OrganizationReportAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+
+    def inspect_organization(self, obj):
+        return format_html(
+            '<a href="../../organizations/organization/{id}/change">inspect organization</a>',
+            id=format(obj.organization_id))
+
+    list_display = ('organization', 'total_urls', 'ok_urls', 'total_endpoints', 'ok_endpoints',
+                    'high', 'medium', 'low', 'ok',
+                    'explained_high', 'explained_medium', 'explained_low', 'when', 'inspect_organization')
+    search_fields = (['organization__name', 'when'])
+    list_filter = ['organization', 'organization__country', 'organization__type__name', 'when'][::-1]
+    # fields = [field.name for field in OrganizationRating._meta.get_fields() if field.name != "id"][::-1]
+
+    fields = ('organization', 'total_urls', 'total_endpoints',
+              'high',
+              'medium',
+              'low',
+              'ok',
+              'high_urls',
+              'medium_urls',
+              'low_urls',
+              'ok_urls',
+              'high_endpoints',
+              'medium_endpoints',
+              'low_endpoints',
+              'ok_endpoints',
+              'total_url_issues',
+              'url_issues_high',
+              'url_issues_medium',
+              'url_issues_low',
+              'url_ok',
+              'total_endpoint_issues',
+              'endpoint_issues_high',
+              'endpoint_issues_medium',
+              'endpoint_issues_low',
+              'endpoint_ok',
+              'explained_high',
+              'explained_medium',
+              'explained_low',
+              'explained_high_urls',
+              'explained_medium_urls',
+              'explained_low_urls',
+              'explained_high_endpoints',
+              'explained_medium_endpoints',
+              'explained_low_endpoints',
+              'explained_total_url_issues',
+              'explained_url_issues_high',
+              'explained_url_issues_medium',
+              'explained_url_issues_low',
+              'explained_total_endpoint_issues',
+              'explained_endpoint_issues_high',
+              'explained_endpoint_issues_medium',
+              'explained_endpoint_issues_low',
+              'when',
+              'calculation')
+
+    ordering = ["-when"]
+
+    save_as = True
--- websecmap/reporting/admin.py
+++ websecmap/reporting/admin.py
@@ -2,73 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from import_export.admin import ImportExportModelAdmin
 
-import websecmap.map.models
 from websecmap.reporting import models
-
-
-@admin.register(websecmap.map.models.OrganizationReport)
-class OrganizationRatingAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    def inspect_organization(self, obj):
-        return format_html(
-            '<a href="../../organizations/organization/{id}/change">inspect organization</a>',
-            id=format(obj.organization_id))
-
-    list_display = ('organization', 'high', 'medium', 'low', 'ok', 'total_urls', 'ok_urls',
-                    'total_endpoints', 'ok_endpoints', 'report', 'explained_high', 'explained_medium',
-                    'explained_low', 'when', 'inspect_organization')
-    search_fields = (['organization__name', 'when'])
-    list_filter = ['organization', 'organization__country', 'organization__type__name', 'when'][::-1]
-    # fields = [field.name for field in OrganizationRating._meta.get_fields() if field.name != "id"][::-1]
-
-    fields = ('organization', 'total_urls', 'total_endpoints',
-              'high',
-              'medium',
-              'low',
-              'ok',
-              'high_urls',
-              'medium_urls',
-              'low_urls',
-              'ok_urls',
-              'high_endpoints',
-              'medium_endpoints',
-              'low_endpoints',
-              'ok_endpoints',
-              'total_url_issues',
-              'url_issues_high',
-              'url_issues_medium',
-              'url_issues_low',
-              'url_ok',
-              'total_endpoint_issues',
-              'endpoint_issues_high',
-              'endpoint_issues_medium',
-              'endpoint_issues_low',
-              'endpoint_ok',
-              'explained_high',
-              'explained_medium',
-              'explained_low',
-              'explained_high_urls',
-              'explained_medium_urls',
-              'explained_low_urls',
-              'explained_high_endpoints',
-              'explained_medium_endpoints',
-              'explained_low_endpoints',
-              'explained_total_url_issues',
-              'explained_url_issues_high',
-              'explained_url_issues_medium',
-              'explained_url_issues_low',
-              'explained_total_endpoint_issues',
-              'explained_endpoint_issues_high',
-              'explained_endpoint_issues_medium',
-              'explained_endpoint_issues_low',
-              'when',
-              'calculation')
-
-    def report(self, obj):
-        return obj
-
-    ordering = ["-when"]
-
-    save_as = True
 
 
 @admin.register(models.UrlReport)
