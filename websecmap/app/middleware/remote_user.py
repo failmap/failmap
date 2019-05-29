47875c9356000985070259ef170e531d97a7e61f
Johan Bloemberg <johan.bloemberg@crv4all.com>
2019-05-29 11:09:42 +0200

    Promote remote users to superuser so they can login to admin

--- websecmap/app/middleware/remote_user.py
+++ websecmap/app/middleware/remote_user.py
@@ -3,3 +3,10 @@ from django.contrib.auth.middleware import RemoteUserMiddleware
 
 class CustomRemoteUserMiddleware(RemoteUserMiddleware):
     header = 'HTTP_REMOTE_USER'
+
+    def process_request(self, request):
+        super().process_request(request)
+        # add staff permissions to newly created remote users
+        if not request.user.is_superuser:
+            request.user.is_superuser = True
+            request.user.save()
