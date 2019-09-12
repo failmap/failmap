ce420892a9b7a435ff4c3fc456f09b694f47f176
stitch1 <elger.jonker@gmail.com>
2019-09-12 12:18:52 +0200

    Lax the timeouts of http connect scans due to false positives.

--- websecmap/scanners/scanner/http.py
+++ websecmap/scanners/scanner/http.py
@@ -62,15 +62,17 @@ http://2.python-requests.org/en/master/user/advanced/?highlight=timeout%3D#timeo
 The connect timeout is the number of seconds Requests will wait for your client to establish a connection to a
 remote machine (corresponding to the connect()) call on the socket. It’s a good practice to set connect timeouts
 to slightly larger than a multiple of 3, which is the default TCP packet retransmission window.
+
+1 second is surely too short, it results in a lot of false positives, especially when scanning a lot of sites.
 """
-CONNECT_TIMEOUT = 1
+CONNECT_TIMEOUT = 10
 
 """
 Once your client has connected to the server and sent the HTTP request, the read timeout is the number of seconds the
 client will wait for the server to send a response. (Specifically, it’s the number of seconds that the client will wait
 between bytes sent from the server. In 99.9% of cases, this is the time before the server sends the first byte).
 """
-READ_TIMEOUT = 1
+READ_TIMEOUT = 10
 
 
 # Discover Endpoints generic task
