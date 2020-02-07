d00435c8c2af4cf45af0984a7ad94fa627b93351
stitch1 <elger.jonker@gmail.com>
2020-02-07 15:53:30 +0100

    Fix bug in best / worst

--- websecmap/map/static/js/components/chart.vue
+++ websecmap/map/static/js/components/chart.vue
@@ -37,9 +37,9 @@
         <div class="row" v-if="filteredData.length > 2">
             <div class="table-responsive">
 
-                <span role="button" class="btn btn-info btn-sm" v-on:click="swapFull()" v-if="filteredData.length > 10">
+                <span role="button" class="btn btn-info btn-sm" v-on:click="swapFull()" v-if="fulldata.length > 10">
                     <svg aria-hidden="true" data-prefix="fas" data-icon="plus-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="" class="svg-inline--fa fa-plus-square fa-w-14"><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-32 252c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92H92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"></path></svg>
-                    {{ $t("chart.expand_list") }} ({{filteredData.length}})
+                    {{ $t("chart.expand_list") }} (+ {{fulldata.length - 10}})
                 </span><br/><br/>
 
                 <table class="table table-striped table-hover" id="chart_table">
