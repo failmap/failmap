5d17838a3532823b725298924e63a21019db048d
stitch1 <elger.jonker@gmail.com>
2020-02-27 16:57:35 +0100

    Add donation drive text and GUI hooks

--- websecmap/map/static/js/components/donation_drive.vue
+++ websecmap/map/static/js/components/donation_drive.vue
@@ -4,63 +4,66 @@
         text-decoration: underline;
     }
 
+    a.btn.btn-primary.lightlink, a.btn.btn-primary.lightlink:hover, a.btn.btn-primary.lightlink:visited, a.btn.btn-primary.lightlink:active {
+        color: white;
+    }
+
 </style>
 
 <template type="x-template" id="donation_drive_template">
     <modal v-if="show_donation_drive" @close="stop()" class="donation_drive">
-            <h3 slot="header">{{ $t("title") }} $CAMPAIGN</h3>
+            <h3 slot="header">Ondersteun basisbeveiliging.nl</h3>
 
             <div slot="body">
 
                 <p>
                     Basisbeveiliging biedt een vrij toegankelijke bron van informatie rondom beveiliging van
-                    de overheid. Dit wordt samengesteld door een team van vrijwilligers.
+                    de overheid. Deze informatie hoort vrij beschikbaar en actueel te zijn.
                 </p>
 
                 <p>
-                    Deze informatie hoort vrij beschikbaar en actueel te zijn. Dat is een behoorlijke klus.
+                    Een team van vrijwilligers vormt, samen met subsidies en donaties, de basis om deze site actueel
+                    te houden. Dat is een behoorlijke klus waar nog verbetering nodig is.
+                </p>
+
+                <h4>Doneren</h4>
+                <p>
+                    Aan organisaties op de kaart vragen we een donatie tussen de &euro;0.25 en &euro;0.50 per domein per maand.
                 </p>
                 <p>
-                    In 2020 willen we meer aandacht besteden aan actuele scanresultaten en afhandelen van meldingen.
-                    Hiervoor willen we minder afhankelijk zijn van vrijwilligers, en meer zekerheid bieden naar de bezoekers.
-                    Doneer aan Basisbeveiliging en help mee aan een veiliger internet.
+                    Gezien budgetten altijd complex zijn kunnen we ook werken met offertes, facturen, codes of een andere weg die bij uw organisatie past. Neem hiervoor <a href="mailto:info@internetcleanup.foundation?subject=donatie"> contact met ons op via deze link</a>.
                 </p>
 
-                <h4>Een gepast bedrag</h4>
                 <p>
-                    Het donatiebedrag is vrij te kiezen. Om het makkelijk te maken suggereren we een donatie.
-                    Als je geen vaste bezoeker bent van de site, en toch het initiatief wil steunen, dan suggereren
-                    we een donatie van 2 euro. <a href="https://useplink.com/payment/qaCyn8t6Tar7c5zVS6Fa/2/">Deze kan je hier overmaken</a>.
+                    Voor fans is het mogelijk om voor ongeveer &euro;5,- per maand bij te dragen.
                 </p>
                 <p>
-                    Organisaties die op deze website worden genoemd vragen we om een bijdrage die bij de grootte van de
-                    organisatie past. Deze bijdrage wordt per organisatie berekend aan de hand van de online voetafdruk.
+                    <a class="btn btn-primary lightlink" style='text-decoration: underline' href="https://www.patreon.com/internetcleanupfoundation?fan_landing=true"><img height='24' src="/static/images/1200px-Patreon_logo.svg.png"> Steun maandelijks via Patreon</a><br>
+                    <br>
+                    <small>Algemene donaties gaan via Patreon voor de <i>Internet Cleanup Foundation</i> en <i>Web Security Map</i>.</small><br>
                 </p>
-                <h4>Voor welke organisatie wil je doneren?</h4>
-                <v-select label="name"
-                          v-model="selected"
-                          :options="possible_organizations"
-                          :placeholder="$t('report.select_organization')"
-                ></v-select>
 
-                <pre>
-                $organization_name              amount  donation    total
-                - Internet addresses:           400     0.20        80
-                - Services:                     780     0.05        39
-                - Scans / metrics:              4680    0.01        46
-                - basic support                 1       100         100
+                <h4>Vrijwilliger worden</h4>
+                <p>
+                    We zijn altijd op zoek naar meer mensen die ervoor zorgen dat de kaart actueel blijft. Hiervoor moet van alles gebeuren.
+                    Ons team is klein en heeft uiteenlopende disciplines. We zijn verenigd in het veiliger maken van het internet.
+                </p>
+                <p>
+                    <a class="btn btn-primary" style='text-decoration: underline' href="mailto:info@internetcleanup.foundation?subject=vrijwilligen">Word vrijwilliger, mail ons</a>
+                </p>
 
-                Suggested donation amount:                          265
-                </pre>
-
-                <p>Met de onderstaande link is het mogelijk om dit bedrag te doneren.</p>
-
-                <button class="btn btn-success" href="https://useplink.com/payment/qaCyn8t6Tar7c5zVS6Fa/265/">Doneer €265</button>
-                (<a href="https://useplink.com/payment/qaCyn8t6Tar7c5zVS6Fa/">of kies zelf een bedrag, hier</a>)
+                <h4>Waarom doneren of vrijwilligen?</h4>
+                <ul>
+                    <li>Het internet aantoonbaar veiliger maken</li>
+                    <li>De kaart up to date houden met dagelijkse en wekelijkse scans</li>
+                    <li>Verbeteren scanners, voor meer accurate resultaten</li>
+                    <li>Oplossen van bugs</li>
+                    <li>Het ontwikkelen van nieuwe functionaliteit</li>
+                </ul>
 
             </div>
             <div slot="footer">
-                <button type="button" class="btn btn-primary" @click="stop()">Bedankt voor je donatie!</button>
+                <button type="button" class="btn btn-secondary" @click="stop()">sluiten</button>
             </div>
         </modal>
 </template>
@@ -69,54 +72,29 @@
 <script>
 const DonationDrive = Vue.component('donation_drive', {
     store,
-    i18n: { // `i18n` option, setup locale info for component
-        messages: {
-            en: {
-                title: "Donation Drive",
-                intro: "",
-            },
-            nl: {
-                title: "Donatieronde",
-                intro: "",
-            }
-        },
-    },
     template: "#donation_drive_template",
-    mixins: [new_state_mixin, translation_mixin],
 
-    mounted: function () {
-        this.load(0)
-    },
+    mounted: function () {},
 
     data: function () {
         return {
-            loading: false,
-
-            show_donation_drive: false,
-            possible_organizations: [],
-
-            selected: {},
 
         }
     },
 
-    props: {},
+    computed: {
+        show_donation_drive: () => {
+            return store.state.show_donation_drive
+        }
+    },
 
     methods: {
-        load: function () {
-            this.loading = true;
-            fetch(`/data/organizations/list/${this.$store.state.country}/${this.$store.state.layer}/`).then(response => response.json()).then(data => {
-                this.possible_organizations = data;
-                this.loading = false;
-            }).catch((fail) => {console.log('An error occurred in combined number statistics: ' + fail); throw fail});
-        },
-
         start: function(){
-            this.show_donation_drive = true;
+            store.commit("show_donation_drive", true);
         },
         stop: function(){
             // reset everything.
-            this.show_donation_drive = false;
+            store.commit("show_donation_drive", false);
         },
 
     },
