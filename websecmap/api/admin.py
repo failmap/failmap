86a27de133001d94b6470c0500510dd61e66dfb8
stitch1 <elger.jonker@gmail.com>
2020-03-12 12:11:52 +0100

    Add searching through API uploads

--- websecmap/api/admin.py
+++ websecmap/api/admin.py
@@ -12,8 +12,8 @@ log = logging.getLogger(__package__)
 @admin.register(models.SIDNUpload)
 class SIDNUpload(ImportExportModelAdmin, admin.ModelAdmin):
 
-    list_display = ('by_user', 'at_when', 'state', 'amount_of_newly_added_domains')
-    search_fields = (['by_user', 'at_when', 'state', 'amount_of_newly_added_domains'])
+    list_display = ('by_user', 'at_when', 'state', 'amount_of_newly_added_domains', 'newly_added_domains')
+    search_fields = (['posted_data'])
     list_filter = ['by_user', 'at_when', 'state', 'amount_of_newly_added_domains'][::-1]
     fields = ('by_user', 'at_when', 'state', 'amount_of_newly_added_domains', 'newly_added_domains', 'posted_data')
 
